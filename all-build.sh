8f20b5a9ebdf77b284ad740595766f7b29dede21
tay3223 <tay3223@qq.com>
2021-11-23 15:14:38 +0800

    v1.0.0

---
+++ all-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+VERSION="v1.0.0"
+
+
+
+CGO_ENABLED=0  GOOS=linux   GOARCH=amd64  go build -o ./build/linux/cdn_refresh -v ./src/main.go
+tar -czvf ./build/linux/cdn-refresh-${VERSION}-linux-amd64.tar.gz ./build/linux/cdn_refresh
+
+
+
+CGO_ENABLED=0  GOOS=darwin  GOARCH=amd64  go build -o ./build/mac/cdn_refresh -v ./src/main.go
+tar -czvf ./build/mac/cdn-refresh-${VERSION}-mac-amd64.tar.gz ./build/mac/cdn_refresh
+
+
+
+CGO_ENABLED=0  GOOS=windows  GOARCH=amd64  go build -o ./build/windows/cdn_refresh.exe -v ./src/main.go
+tar -czvf ./build/windows/cdn-refresh-${VERSION}-windows-amd64.tar.gz ./build/windows/cdn_refresh.exe
+
+
